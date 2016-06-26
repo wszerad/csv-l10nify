f5c4c826f5277598ef5ba7b39a4b23ab081923d6
wszerad <wszerad91@gmail.com>
2016-06-26 22:50:51 +0200

    init

--- README.md
+++ README.md
@@ -1,76 +1,22 @@
-# csv-l10n
-Simple engine for l10n (provided via csv files)
+# csv-l10nify
+csv-l10n for browserify
 
 ```
-npm install csv-l10n
+npm install csv-l10nify
 ```
-
-### csv table design
-| key\lang (this one is unused) | en  | de  |
-|---|---|---|
-| hello | Hello | Hallo |
-| guys | guys | Jungs  |
-| template | My name is ${0}, I am ${1} | Ich bin ${1} und ich heiße ${0}  |
-
-You can use templates inside resorces (like in last row). Templates are filled with arguments wrapped in 'bindingStart' and 'bindingEnd' options (see below)
-
-### constructor L10n
-#### L10n(path[, options, cb])
-* path - absolute path to csv file
-* options:
-```
-{ //defaults
-  blockStart: '__(',  //start marks inside text to parse
-  blockEnd: ')',  //end marks inside text to parse
-  templateStart: '${', //start marks inside resorce
-  templateEnd: '}', //end marks inside resorce
-  bindingStart: '{{',  //start marks for front, here for Angular
-  bindingEnd: '}}'   //end marks for front, here for Angular
-}
-```
-* cb - if not specified lang file will by loaded sync
-**return l10n instance**
-
-### l10n instance methods
-#### l10n.render(text[, lang])
-* text - to localization
-* lang - default first lang (second csv column)
-return localized text
-
-#### l10n.reload(cb)
-reload csv file (async), use render again to use updated resorces.
-
-#### l10n.reloadSync()
-reload csv file (sync), use render again to use updated resorces. 
-
-#### l10n.setLang()
-reload set lang to use by default.
-
-#### l10n.langs
-array of loaded langs
-
-#### l10n.errors
-array of unknown resources (cleaned befor each use of l10n.render)
-
-#### l10n.count
-number of injections during rendering
 
 ### Usage:
 ```js
-var path = require('path'),
-    CSVPath = path.join(__dirname, './lang.csv'), //absolute path to csv file
-    options = { //default options
-      blockStart: '__(',
-  		blockEnd: ')',
-  		templateStart: '${',
-        templateEnd: '}',
-  		bindingStart: '{{',
-  		bindingEnd: '}}'
-    },
-    l10n = require('csv-l10n')(CSVPath, options);
+var browserify = require('browserify');
+var fs = require('fs');
 
-var strToLocalize = '__(hello) __(guys)!. Angular template: __(template, name, age)',
-    afterLocalize = l10n.render(strToLocalize, 'de');
+var opt = {};	//csv-l10n options
+var csvL10nify = require('csv-l10nify')('./langFile.csv', opt);
 
-console.log(afterLocalize); //--> Hallo Jungs! Angular template: Ich bin {{age}} und ich heiße {{name}}.
+var b = browserify({
+	entries: ['src/main.js'],
+	transform: [csvL10nify.transform]
+});
+
+b.bundle().pipe(fs.createWriteStream('dist/build.js'));
 ```
--- index.js
+++ index.js
@@ -1,142 +1,31 @@
 'use strict';
 
-var fs = require('fs'),
-	parse = require('csv-parse/lib/sync');
+var through = require('through');
+var L10n = require('csv-l10n');
 
-function L10n(path, opt, cb) {
-	if(typeof opt === 'function') {
-		cb = opt;
-		opt = {};
-	}
+module.exports = function (langFile, opt) {
+	var l10n = L10n(langFile, opt);
 
-	opt = opt || {};
-	this.path = path;
-	this.blocks = {
-		blockStart: opt.blockStart || '__(',
-		blockEnd: opt.blockEnd || ')',
-		templateStart:  opt.templateStart || '${',
-		templateEnd: opt.templateEnd || '}',
-		bindingStart: opt.bindingStart || '{{',
-		bindingEnd: opt.bindingEnd || '}}'
+	l10n.transform = function (file) {
+		var data = '';
+		var stream = through(write, end);
+
+		function write(buf) {
+			data += buf
+		}
+
+		function end() {
+			var results = l10n.render(data);
+
+			if(l10n.count)
+				stream.emit('file', langFile);
+
+			stream.queue(results);
+			stream.queue(null);
+		}
+
+		return stream;
 	};
-	this.__blockRegExp = new RegExp('(' + regExpEscape(this.blocks.blockStart) + '.+?' + regExpEscape(this.blocks.blockEnd) + ')', 'g');
-	this.__templateRegExp = new RegExp('(' + regExpEscape(this.blocks.templateStart) + '\\d+' + regExpEscape(this.blocks.templateEnd) + ')', 'g');
-	this.errors = [];
-	this.langs = [];
-	this.count = 0;
-	this.lang = null;
-	this.count = 0;
 
-	if(cb) {
-		this.reload(cb);
-	} else {
-		this.reloadSync();
-	}
-}
-L10n.prototype = {
-	render: function(str, lang) {
-		this.errors.length = 0;
-		this.count = 0;
-
-		if(lang) {
-			this.lang = lang;
-		} else if(this.lang === null && !this.langs.length) {
-			throw new Error('No language file detected');
-		} else if(this.lang === null) {
-			this.lang = this.langs[0];
-		}
-
-		return str.replace(this.__blockRegExp, this.injector.bind(this));
-	},
-
-	setLang: function(lang) {
-		if(this.langs.indexOf(lang) !== -1) {
-			this.lang = lang;
-		} else {
-			throw new Error('Non-existent language: '+lang);
-		}
-
-		return this;
-	},
-
-	reload: function(cb) {
-		var self = this;
-
-		fs.readFile(self.path, function(err, data) {
-			if(err)
-				return cb(err);
-
-			self.reloadSync(parse(data));
-
-			cb(null);
-		});
-	},
-
-	reloadSync: function(csv) {
-		var self = this,
-			data = csv || parse(fs.readFileSync(self.path));
-
-		var column = data[0],
-			langs = [],
-			ret = {};
-
-		for(var i=1; i<column.length; i++) {
-			langs.push(column[i]);
-		}
-
-		self.langs = langs;
-		self.resources = langs.reduce(function(ret, lang, index) {
-			var clang = ret[lang] = {},
-				position = index+1;
-
-			for(var j=1; j<data.length; j++) {
-				clang[data[j][0]] = self.parseTemplate(data[j][position]);
-			}
-
-			return ret;
-		}, ret);
-	},
-
-	parseTemplate: function(str) {
-		var self = this,
-			ret = {};
-
-		ret.parts = str.split(self.__templateRegExp);
-		ret.parts.forEach(function(part, index) {
-			if(self.__templateRegExp.test(part)) {
-				var num = part.slice(self.blocks.templateStart.length, self.blocks.templateEnd.length*-1);
-				ret[num] = index;
-			}
-		});
-
-		return ret;
-	},
-
-	injector: function(str) {
-		var self = this,
-			args = str.replace(/\s/g, '').slice(self.blocks.blockStart.length, self.blocks.blockEnd.length*-1).split(','),
-			lang = self.resources[self.lang],
-			template = lang[args[0]];
-
-		self.count++;
-
-		if(template === undefined) {
-			self.errors.push(args[0]);
-			return args[0];
-		}
-
-		args.slice(1).forEach(function(arg, index){
-			template.parts[template[index]] = self.blocks.bindingStart+arg+self.blocks.bindingEnd;
-		});
-
-		return template.parts.join('');
-	}
-};
-
-function regExpEscape(str) {
-	return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-}
-
-module.exports = function(path, opt, cb) {
-	return new L10n(path, opt, cb);
+	return l10n;
 };
